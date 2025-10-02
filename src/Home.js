41acba927e65eaccb46ffb45846cb3a5cf49387b
Olutayo Kevin Akinkumi <olutayo404@gmail.com>
2025-10-02 15:24:00 +0100

    5th commit

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Home = () => {
 // let name = 'mario';
 const [name, setName] = useState('mario');
+const [age, setAge] = useState(25)
 
 const handleClick = () => {  
     setName('luigi');
+    setAge(30);
 }
 
     return (
         <div className="Home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
      );
