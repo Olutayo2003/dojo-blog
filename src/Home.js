a06a995e581aa51aa6fba1e9cab18ed6dff04fbc
Olutayo Kevin Akinkumi <olutayo404@gmail.com>
2025-10-02 14:51:22 +0100

    added an onclick event

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+// let name = 'mario';
+const [name, setName] = useState('mario');
 
-const handleClick = () => {
-    console.log('hello, ninjas')
+const handleClick = () => {  
+    setName('luigi');
 }
 
-const handleClickAgain = (name) => {
-    console.log('hello ' + name);
-}
-
-    return ( 
+    return (
         <div className="Home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => {
-               handleClickAgain('mario')
-            }}>Click me again</button>
         </div>
      );
 }
